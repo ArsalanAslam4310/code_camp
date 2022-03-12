9ec3ac77ccec9f83f8a936d1e9d278cfee1cc3eb
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-03-12 07:30:34 +0500

    Add files via upload

---
+++ first_lst_name.py
@@ -0,0 +1,8 @@
+def first_last_name(lis):
+    new=[]
+    g={'first_name':input("Enter a first name"),"second_name":input("Enter a second name ")}
+    new.append(g)
+    return new
+
+lis=[]
+print(first_last_name(lis))
