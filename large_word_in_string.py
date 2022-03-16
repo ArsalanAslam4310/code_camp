b6b07fd9de1c9dcea7546ab897a8c4cd26f26898
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-03-16 23:02:29 +0500

    Create large_word_in_string.py

---
+++ large_word_in_string.py
@@ -0,0 +1,11 @@
+def large_word(string):
+    maximum = 0
+
+    for i in range(len(string)):
+        if __name__=="__i__":
+            if maximum > string[i]:
+                maximum= string[i]
+    return i
+
+stri="dbxjh hdbxjhnb jasbxnxb xbzjhbsdjhxgujhdb dbxn"
+print(large_word(stri))
