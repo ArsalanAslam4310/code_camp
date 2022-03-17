4480f9ba7eb55fa673f455fad0b518e8c2d0d460
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-03-17 11:01:52 +0500

    Update large_word_in_string.py

--- large_word_in_string.py
+++ large_word_in_string.py
@@ -1,11 +1,20 @@
 def large_word(string):
     maximum = 0
+    sub_stri_leng = 0
+    j = 0
 
     for i in range(len(string)):
-        if __name__=="__i__":
-            if maximum > string[i]:
-                maximum= string[i]
-    return i
+        j = i
+        sub_stri_leng = 0
 
-stri="dbxjh hdbxjhnb jasbxnxb xbzjhbsdjhxgujhdb dbxn"
+        while string[j] != " " and j < len(string)-1:
+            j += 1
+            sub_stri_leng += 1
+            if sub_stri_leng > maximum:
+                maximum = sub_stri_leng
+
+    return maximum
+
+
+stri = "dbxjh hdbxjhnb jasbxnxb xbzjhbsdjhxgujhdb dbxn"
 print(large_word(stri))
