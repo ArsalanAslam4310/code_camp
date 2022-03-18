c9efb9733c3593cca102e761fb0610732f719179
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-03-18 23:15:59 +0500

    Update max_in_sub_list.py

--- max_in_sub_list.py
+++ max_in_sub_list.py
@@ -1,12 +1,12 @@
 def maximum(lis):
     max = 0
     new=[]
-
     for i in range(len(lis)):
-        if max < lis[i][1]:
-            max = lis[i][1]
-            new.append(max)
+        for j in lis[i]:
+            if max < j:
+                max = j
+                new.append(max)
     return new
 
-li=[[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]
+li=[[111, 2, 11, 3], [133, 27, 18, 26], [323, 35, 37, 39], [10003, 1001, 857, 1]]
 print(maximum(li))
