080efa15274a85a7e712a1d2b020f9fe6c4dc7da
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-03-23 22:16:54 +0500

    Update new_method.py

--- new_method.py
+++ new_method.py
@@ -1,15 +1,14 @@
-def confirm_ending(string,target):
-
+def confirm_the_ending(string,target):
+    j = len(target)-1
+    j -= 1
+    
     for i in range(len(string)-1,-1,-1):
-        i -= 1
-        for j in range(len(target)-1,-1,-1):
-            j -= 1
-            if string[i]==target[j]:
-                return True
-            else:
-                return False
-            
-        
-string="fbhdvkbfkc jdnck"
-target=" jdnck"
-print(confirm_ending(string,target))
+        if string[i]==target[j]:
+            return True
+        else:
+            return False
+
+
+string=["asadfre hg"]
+target= ["hjg"]
+print(confirm_the_ending(string,target))
