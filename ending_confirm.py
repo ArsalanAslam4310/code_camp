4fe1dca69f21957c1bfe2a8d0e7be239b41ae4dc
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-03-25 10:42:53 +0500

    Update ending_confirm.py

--- ending_confirm.py
+++ ending_confirm.py
@@ -1,14 +1,17 @@
-def confirm_the_ending(string,target):
-    for j in range(-1,-len(string),-1):
-        print(string[j])
-        for i in range(-1,-len(target),-1):
-            if target[i]==string[j]:
-                return True
-            else:
-                return False
-        
+def confirm_the_ending(string, target):
+    flag = True
+    for j in range(-1, -(len(target))-1, -1):
+        if target[j] == string[j]:
+            flag = True
+        else:
+            flag = False
+            break
+    if flag:
+        return flag
+    return flag
 
 
-string=["asadfre hg"]
-target= ["g"]
-print(confirm_the_ending(string,target))
+string = "asadfre hg"
+target = "  g"
+print(len(target))
+print(confirm_the_ending(string, target))
