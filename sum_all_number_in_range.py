713b8c59d32b1c81620c1a8ce96170756ee1ef33
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-03-27 22:31:49 +0500

    Create sum_all_number_in_range.py

---
+++ sum_all_number_in_range.py
@@ -0,0 +1,10 @@
+def sum_number(lis):
+    '''sum of all number in list between two index
+    '''
+    sum =0
+    for i in range(lis[0],lis[0]):
+        sum = sum +i
+        i = i +1
+    return sum 
+
+print(sum_number([5,10]))
